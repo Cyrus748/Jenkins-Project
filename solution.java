3f0e22a726fd2264934f69e94d7104d92920d201
Aditya Negi <119617732+Cyrus748@users.noreply.github.com>
2024-08-16 11:00:24 +0530

    UPLOADED SUCESSFULLY

---
+++ solution.java
@@ -0,0 +1,7 @@
+class solution {
+    public static void main(String [] args){
+        System.out.println("Hello Duniya");
+        System.out.println("THIS IS JENKINS TESTING");
+        
+    }
+}
